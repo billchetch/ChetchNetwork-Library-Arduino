1374792eb5d9f47ddb7bf5c163faa768ed21c987
billchetch <45006134+billchetch@users.noreply.github.com>
2021-10-11 23:51:53 +0800

    s

--- src/ChetchEthernetManager..cpp
+++ src/ChetchEthernetManager..cpp
@@ -9,7 +9,12 @@ bool EthernetManager::begin(byte* mac, int timeout){
     
     unsigned long started = millis();
 
-    if(trace)Serial.println("Starting ethernet...");
+    if(trace){
+        Serial.print("Starting ethernet with mac");
+        char macAddr[18];
+        sprintf(macAddr, "%2X:%2X:%2X:%2X:%2X:%2X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+        Serial.println(macAddr);
+    }
     Ethernet.begin(mac); //, ip, myDns, gateway, subnet);
 
   	// Check for Ethernet hardware present
