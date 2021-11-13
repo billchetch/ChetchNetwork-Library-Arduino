19a1c2b21ac12353191c009d60fb8eba6fafbfd9
billchetch <45006134+billchetch@users.noreply.github.com>
2021-11-13 23:41:22 +0800

    s

--- src/ChetchEthernetManager..cpp
+++ src/ChetchEthernetManager..cpp
@@ -5,7 +5,7 @@ namespace Chetch{
 bool EthernetManager::trace = false;
 
 //TODO: implement timeout
-bool EthernetManager::begin(byte* mac, int timeout){
+bool EthernetManager::begin(byte* mac, byte* ip, byte* dns, byte* gateway, byte* subnet, int timeout){
     
     unsigned long started = millis();
 
@@ -15,7 +15,7 @@ bool EthernetManager::begin(byte* mac, int timeout){
         sprintf(macAddr, "%2X:%2X:%2X:%2X:%2X:%2X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
         Serial.println(macAddr);
     }
-    Ethernet.begin(mac); //, ip, myDns, gateway, subnet);
+    Ethernet.begin(mac, ip, dns, gateway, subnet);
 
   	// Check for Ethernet hardware present
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
@@ -25,17 +25,11 @@ bool EthernetManager::begin(byte* mac, int timeout){
         if(trace)Serial.println("Hardware connection ok");
     }
 
-    if (Ethernet.linkStatus() == LinkOFF) {
-        if(trace)Serial.println("Ethernet cable is not connected.");
-        return false;
-    } else {
-        if(trace){
-            Serial.println("Link status on");
-            Serial.println("Local IP: ");
-            Serial.println(Ethernet.localIP());
-        }
-        return true;
+    if(trace){}
+        Serial.println("Local IP: ");
+        Serial.println(Ethernet.localIP());
     }
+    return true;
 }
 
 } //end namespace
--- src/ChetchEthernetManager.h
+++ src/ChetchEthernetManager.h
@@ -10,7 +10,7 @@ namespace Chetch{
 class EthernetManager{
     public:
         static bool trace;
-        static bool begin(byte* mac, int timeout = -1);        ;
+        static bool begin(byte* mac, byte* ip, byte* dns, byte* gateway, byte* subnet, int timeout = -1);
 };
 
 } //end namespace
